328a03054824568bdef5eda6250fe333dff2ccb5
Sandy Cash <lhcash@us.ibm.com>
2023-09-10 13:51:45 -0400

    Working code to generate data sets - Run "go build" in the generator subdir, then run that using the sample config

--- generator/generator.go
+++ generator/generator.go
@@ -1,29 +1,31 @@
-package generator
+package main
 
 import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 
 	"github.com/ScarletTanager/basilisk/classifiers"
+	"github.com/ScarletTanager/wyvern"
 )
 
 var (
-	configPath                       string
-	outputPath                       string
+	configPath string
+	outputPath string
 )
 
 type DataSetAttribute struct {
 	Name                  string    `json:"name"`
-	LowerBound            float64   `json:"lowerBound"`
-	UpperBound            float64   `json:"upperBound"`
+	LowerBound            float64   `json:"lower"`
+	UpperBound            float64   `json:"upper"`
 	AllocationsByQuintile []float64 `json:"allocationsByQuintile"`
 }
 
 type DatasetConfig struct {
-	Classes    map[string][]DataSetAttribute `json:"classes"`
-	EntryCount uint64                        `json:"entryCount"`
+	Classes     map[string][]DataSetAttribute `json:"classes"`
+	RecordCount int                           `json:"recordCount"`
 }
 
 func init() {
@@ -53,7 +55,7 @@ func main() {
 	}
 
 	classNames := make([]string, 0)
-	for k, v := range datasetConfig.Classes {
+	for k, _ := range datasetConfig.Classes {
 		classNames = append(classNames, k)
 	}
 
@@ -64,6 +66,135 @@ func main() {
 
 	// Generate the data
 	// This is a crappy way to do this if the number of records is really large
-	records := make([]classifiers.Record, datasetConfig.EntryCount)
-	for i := 0
+	records := make([]classifiers.Record, datasetConfig.RecordCount)
+	for ri := range records {
+		records[ri] = classifiers.Record{
+			AttributeValues: make(wyvern.Vector[float64], len(attributeNames)),
+		}
+	}
+
+	// Assign each record index a random class
+	recordIndicesByClass := assignClasses(datasetConfig.RecordCount, len(classNames))
+
+	// Compute the attribute values for each class
+	// recordsInClass is []int - values are indices into the _overall_ list of records from 0 to datasetConfig.RecordCount
+	for classIdx, recordsInClass := range recordIndicesByClass {
+		// Populate the class in the overall record table
+		// TODO: we could do this more cleanly by having a single method
+		// generate the list of records with the classes assigned
+		for _, ri := range recordsInClass {
+			records[ri].Class = classIdx
+		}
+
+		// Iterate over attributes on a per-class basis
+		for attrIdx, attr := range datasetConfig.Classes[classNames[classIdx]] {
+			// For each attribute, compute a quintile distribution for the current class
+			// Assign records to quintiles according to the computed distribution
+			// Each bucket is []int, values are the original indices into the overall record set (values of recordsInClass)
+			countsByQuintile := computeQuintileDistribution(len(recordsInClass), attr.AllocationsByQuintile)
+			recordsByQuintile := assignQuintiles(recordsInClass, countsByQuintile)
+			for qi, quintile := range recordsByQuintile {
+				for _, r := range quintile {
+					val := computeAttributeValue(attr.LowerBound, attr.UpperBound, qi)
+					records[r].AttributeValues[attrIdx] = val
+				}
+			}
+		}
+	}
+
+	dataset, err := classifiers.NewDataSet(classNames, attributeNames, records)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error generating dataset: %s\n", err)
+		os.Exit(1)
+	}
+
+	datasetBytes, err := json.Marshal(dataset)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error marshaling dataset: %s\n", err)
+		os.Exit(1)
+	}
+
+	f, err := os.OpenFile(outputPath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening output file: %s\n", err)
+		os.Exit(1)
+	}
+
+	_, err = f.Write(datasetBytes)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing output data: %s\n", err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
+
+// Returns slice of slice(int) - outer indices are class indices, inner indices
+// range up to the count of records assigned each class, inner values
+// are the actual record indices assigned to each class
+func assignClasses(recordCount, classCount int) [][]int {
+	recordIndicesByClass := make([][]int, classCount)
+	for i, _ := range recordIndicesByClass {
+		recordIndicesByClass[i] = make([]int, 0)
+	}
+
+	for i := 0; i < recordCount; i++ {
+		assignedClass := rand.Intn(classCount)
+		recordIndicesByClass[assignedClass] = append(recordIndicesByClass[assignedClass], i)
+	}
+
+	return recordIndicesByClass
+}
+
+// Given a set of record indices ([]int), assign them to quintiles according to quintileCounts
+func assignQuintiles(records []int, quintileCounts []int) [][]int {
+	quintileRecords := make([][]int, 5)
+	for i := range quintileRecords {
+		quintileRecords[i] = make([]int, 0)
+	}
+
+	bucket := 0
+	for ri := 0; ri < len(records); ri++ {
+		// Yeah, risky
+		// Find the first quintile which is not at capacity, starting with the one
+		// to which we most recently added.
+		for b := bucket; ; b++ {
+			quintileIdx := b % 5
+			if len(quintileRecords[quintileIdx]) < quintileCounts[quintileIdx] {
+				quintileRecords[quintileIdx] = append(quintileRecords[quintileIdx], records[ri])
+				bucket = b
+				break
+			}
+		}
+	}
+
+	return quintileRecords
+}
+
+func computeQuintileDistribution(recordCount int, allocations []float64) []int {
+	recordCountsByQuintile := make([]int, 5)
+	for i, a := range allocations {
+		recordCountsByQuintile[i] = int((a / 100) * float64(recordCount))
+	}
+
+	tot := 0
+	for _, rc := range recordCountsByQuintile {
+		tot += rc
+	}
+
+	if tot < recordCount {
+		for i := 0; i < (recordCount - tot); i++ {
+			recordCountsByQuintile[i%5] += 1
+		}
+	} else if tot > recordCount {
+		for i := 0; i < (tot - recordCount); i++ {
+			recordCountsByQuintile[i%5] -= 1
+		}
+	}
+
+	return recordCountsByQuintile
+}
+
+func computeAttributeValue(lower, upper float64, quintile int) float64 {
+	return ((upper - lower) * (float64((quintile*20)+rand.Intn(21)) / 100.0)) + lower
 }
