be9813612c1ab3521450f3b3d8922ddcb0b5a346
Sandy Cash <lhcash@us.ibm.com>
2024-04-19 20:01:16 -0400

    WIP2

--- classifiers/naivebayes.go
+++ classifiers/naivebayes.go
@@ -1,7 +1,9 @@
 package classifiers
 
 import (
+	"errors"
 	"fmt"
+	"math"
 
 	"github.com/ScarletTanager/sphinx/probability"
 )
@@ -12,6 +14,7 @@ import (
 
 type NaiveBayesClassifier struct {
 	ClassifierImplementation
+	VectorConditionedClassProbabilities [][]float64
 }
 
 const (
@@ -57,32 +60,80 @@ func (nbc *NaiveBayesClassifier) train(cfg *DataSplitConfig) error {
 	// Calculate the prior probability of each class
 	// This should probably be a method on the DataSet...?
 
-	// Use the raw data in case the split left some classes out of the training dataset
-	classPriors := make([]float64, len(nbc.RawData.ClassNames))
-
-	// Stick the classes into a slice of ints
-	trainingClasses := make([]int, len(nbc.TrainingData.Records))
-	for i, rec := range nbc.TrainingData.Records {
-		trainingClasses[i] = rec.Class
-	}
-
-	// Compute the class priors
-	classPriorPMF := probability.MassDiscrete(trainingClasses)
-	for i, _ := range nbc.RawData.ClassNames {
-		classPriors[i] = classPriorPMF(i)
-	}
+	classPriors := computeClassPriors(nbc.RawData.ClassNames, nbc.TrainingData.Records)
 
 	// Discretize the attributes - we will use the output from this step
 	// to calculate the conditional probability P(X==x|C==c), where x is the vector of attributes,
 	// and c is the class.
 
-	attributeIntervals := make([]probability.Intervals, len(nbc.TrainingData.AttributeNames))
+	attributeIntervals := DiscretizeAttributes(nbc.RawData.AttributeNames, nbc.TrainingData.Records)
+
+	// Now we need to calcuate P(An==a|C==c) (A1...An are the individual attribute variables)
+
+	// For each class, for each attribute, we compute the probability for
+	// each interval
+
+	caps := GenerateClassAttributePosteriors(nbc.RawData.ClassNames, nbc.RawData.AttributeNames,
+		attributeIntervals, nbc.TrainingData.Records)
+
+	// Next calculate P(X==x|C==c)
+
+	ccps := GenerateClassConditionedPosteriors(nbc.RawData.ClassNames, nbc.RawData.AttributeNames,
+		attributeIntervals, caps)
+
+	// Aaaaand...apply Bayes to get P(C==c|X==x)
+
+	vectorCount := len(ccps[0])
+
+	nbc.VectorConditionedClassProbabilities = make([][]float64, vectorCount)
+	for v, _ := range nbc.VectorConditionedClassProbabilities {
+		nbc.VectorConditionedClassProbabilities[v] = make([]float64, len(nbc.RawData.ClassNames))
+	}
+
+	vectorTotalProbabilities := make([]float64, vectorCount)
+	for i, _ := range vectorTotalProbabilities {
+		for classIdx, _ := range nbc.RawData.ClassNames {
+			vectorTotalProbabilities[i] += ccps[classIdx][i] * classPriors[classIdx]
+		}
+	}
+
+	for classIdx, _ := range ccps {
+		for vectorIdx, pVal := range ccps[classIdx] {
+			nbc.VectorConditionedClassProbabilities[vectorIdx][classIdx], _ = probability.Bayes(classPriors[classIdx],
+				pVal, vectorTotalProbabilities[vectorIdx])
+		}
+	}
+
+	return nil
+}
+
+func computeClassPriors(classNames []string, records []Record) []float64 {
+	// Use the raw data in case the split left some classes out of the training dataset
+	classPriors := make([]float64, len(classNames))
+
+	// Stick the classes into a slice of ints
+	trainingClasses := make([]int, len(records))
+	for i, rec := range records {
+		trainingClasses[i] = rec.Class
+	}
+
+	// Compute the class priors and cache them
+	classPriorPMF := probability.MassDiscrete(trainingClasses)
+	for i, _ := range classNames {
+		classPriors[i] = classPriorPMF(i)
+	}
+
+	return classPriors
+}
+
+func DiscretizeAttributes(attributeNames []string, records []Record) []probability.Intervals {
+	attributeIntervals := make([]probability.Intervals, len(attributeNames))
 
 	// Range over the attributes
-	for attrIdx, _ := range nbc.TrainingData.AttributeNames {
-		// Range over the records to find min and max values
-		vals := make([]float64, len(nbc.TrainingData.Records))
-		for i, rec := range nbc.TrainingData.Records {
+	for attrIdx, _ := range attributeNames {
+		// Range over the records, store attribute-specific slices of values
+		vals := make([]float64, len(records))
+		for i, rec := range records {
 			vals[i] = rec.AttributeValues[attrIdx]
 		}
 
@@ -90,56 +141,105 @@ func (nbc *NaiveBayesClassifier) train(cfg *DataSplitConfig) error {
 		attributeIntervals[attrIdx] = probability.Discretize(vals, probability.DiscretizationConfig{})
 	}
 
-	// Now we need to calcuate P(X==x|C==c)
+	return attributeIntervals
+}
 
-	// For each class, for each attribute, we compute the probability for
-	// each interval
+// ClassAttributePosteriors is just a convenience type alias to make retrieving the conditional
+// probabilities easier
+type ClassAttributePosteriors [][][]float64
 
+func (cap ClassAttributePosteriors) ClassAttributePosterior(classIdx, attrIdx, intervalIdx int) (float64, error) {
+	if cap[classIdx] == nil || cap[classIdx][attrIdx] == nil {
+		return 0.0, errors.New("unable to retrieve probability, storage has not been initialized")
+	}
+
+	return cap[classIdx][attrIdx][intervalIdx], nil
+}
+
+// GenerateClassAttributePosteriors computes, for each class and attribute, the conditional probability
+// that the attribute has a value in each interval, given the class.
+func GenerateClassAttributePosteriors(classNames, attributeNames []string, intervals []probability.Intervals, records []Record) ClassAttributePosteriors {
 	// Create the probability storage
-	classAttributePosteriors := make([][][]float64, len(nbc.RawData.ClassNames))
-	for i, _ := range classAttributePosteriors {
-		classAttributePosteriors[i] = make([][]float64, len(nbc.RawData.AttributeNames))
-		for j, _ := range classAttributePosteriors[i] {
-			classAttributePosteriors[i][j] = make([]float64, len(attributeIntervals[j]))
+	caps := make(ClassAttributePosteriors, len(classNames))
+
+	for i, _ := range caps {
+		caps[i] = make([][]float64, len(attributeNames))
+		for j, _ := range caps[i] {
+			caps[i][j] = make([]float64, len(intervals[j]))
 		}
 	}
 
 	// Now convert the attribute values to intervals, first
 	// create some storage for the interval values themselves
-	classAttrIntervals := make([][][]int, len(nbc.RawData.ClassNames))
-	for classIdx, _ := range nbc.RawData.ClassNames {
-		classAttrIntervals[classIdx] = make([][]int, len(nbc.RawData.AttributeNames))
-		for attrIdx, _ := range nbc.RawData.AttributeNames {
-			classAttrIntervals[classIdx][attrIdx] = make([]int, 0)
+	cais := make([][][]int, len(classNames))
+	for classIdx, _ := range cais {
+		cais[classIdx] = make([][]int, len(attributeNames))
+		for attrIdx, _ := range cais[classIdx] {
+			cais[classIdx][attrIdx] = make([]int, 0)
 		}
 	}
 
 	// Do the conversion and store the values
-	for _, rec := range nbc.TrainingData.Records {
-		for attrIdx, _ := range nbc.TrainingData.AttributeNames {
-			classAttrIntervals[rec.Class][attrIdx] = append(classAttrIntervals[rec.Class][attrIdx],
-				attributeIntervals[attrIdx].IntervalForValue(rec.AttributeValues[attrIdx]))
+	for _, rec := range records {
+		for attrIdx, _ := range attributeNames {
+			cais[rec.Class][attrIdx] = append(cais[rec.Class][attrIdx],
+				intervals[attrIdx].IntervalForValue(rec.AttributeValues[attrIdx]))
 		}
 	}
 
 	// Create the PMFs
-	classAttrPMFs := make([][]probability.ProbabilityMassFunction, len(nbc.RawData.ClassNames))
+	classAttrPMFs := make([][]probability.ProbabilityMassFunction, len(classNames))
 	for classIdx, _ := range classAttrPMFs {
-		classAttrPMFs[classIdx] = make([]probability.ProbabilityMassFunction, len(nbc.RawData.AttributeNames))
+		classAttrPMFs[classIdx] = make([]probability.ProbabilityMassFunction, len(attributeNames))
 		for attrIdx, _ := range classAttrPMFs[classIdx] {
-			classAttrPMFs[classIdx][attrIdx] = probability.MassDiscrete(classAttrIntervals[classIdx][attrIdx])
+			classAttrPMFs[classIdx][attrIdx] = probability.MassDiscrete(cais[classIdx][attrIdx])
 		}
 	}
 
 	// Compute the class-conditioned posteriors for each individual attribute
-	for classIdx, _ := range classAttributePosteriors {
-		for attrIdx, _ := range classAttributePosteriors[classIdx] {
-			for intervalIdx, _ := range classAttributePosteriors[classIdx][attrIdx] {
-				classAttributePosteriors[classIdx][attrIdx][intervalIdx] =
+	for classIdx, _ := range caps {
+		for attrIdx, _ := range caps[classIdx] {
+			for intervalIdx, _ := range caps[classIdx][attrIdx] {
+				caps[classIdx][attrIdx][intervalIdx] =
 					classAttrPMFs[classIdx][attrIdx](intervalIdx)
 			}
 		}
 	}
 
-	return nil
+	return caps
+}
+
+type ClassConditionedPosteriors [][]float64
+
+func GenerateClassConditionedPosteriors(classNames, attributeNames []string,
+	intervals []probability.Intervals, caps ClassAttributePosteriors) ClassConditionedPosteriors {
+	// We use 10 intervals per attribute, if we make that configurable,
+	// then the base of the exponential must be the interval count
+	vectorCount := int(math.Pow10(len(attributeNames)))
+
+	ccps := make(ClassConditionedPosteriors, len(classNames))
+	for classIdx, _ := range ccps {
+		ccps[classIdx] = make([]float64, vectorCount)
+		for i, _ := range ccps[classIdx] {
+			ccps[classIdx][i] = 1.0
+		}
+
+		for attrIdx, _ := range attributeNames {
+			chunkSize := int(math.Pow10(len(attributeNames) - 1 - attrIdx))
+			vectorIdx := 0
+			for vectorIdx < vectorCount {
+				// If we ever allow the interval count to be configurable, this will need to change
+				for k, _ := range intervals[attrIdx] {
+					pVal, _ := caps.ClassAttributePosterior(classIdx, attrIdx, k)
+					// Multiply the probability of every vector in the chunk by the interval posterior
+					for chunkSlotIdx := 0; chunkSlotIdx < chunkSize; chunkSlotIdx++ {
+						ccps[classIdx][vectorIdx] = ccps[classIdx][vectorIdx] * pVal
+						vectorIdx++
+					}
+				}
+			}
+		}
+	}
+
+	return ccps
 }
--- classifiers/naivebayes_test.go
+++ classifiers/naivebayes_test.go
@@ -5,9 +5,11 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/ScarletTanager/basilisk/classifiers"
+	"github.com/ScarletTanager/sphinx/probability"
+	"github.com/ScarletTanager/wyvern"
 )
 
-var _ = Describe("Naivebayes", func() {
+var _ = FDescribe("Naivebayes", func() {
 	var (
 		nbc  *classifiers.NaiveBayesClassifier
 		cfg  *classifiers.DataSplitConfig
@@ -19,7 +21,7 @@ var _ = Describe("Naivebayes", func() {
 		cfg = nil
 	})
 
-	FContext("TrainFromCSVFile", func() {
+	Context("TrainFromCSVFile", func() {
 		BeforeEach(func() {
 			path = "../datasets/shorebirds.csv"
 		})
@@ -36,6 +38,360 @@ var _ = Describe("Naivebayes", func() {
 			It("Returns an error", func() {
 				Expect(nbc.TrainFromCSVFile(path, cfg)).To(HaveOccurred())
 			})
+		})
+	})
+
+	Describe("DiscretizeAttributes", func() {
+		var (
+			attributeNames []string
+			records        []classifiers.Record
+		)
+
+		BeforeEach(func() {
+			attributeNames = []string{"a", "b"}
+			records = []classifiers.Record{
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{5.0, 5.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{13.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{20.0, 7.5},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{9.8, 10.0},
+				},
+			}
+		})
+
+		It("Creates a set of intervals for each attribute", func() {
+			attributeIntervals := classifiers.DiscretizeAttributes(attributeNames, records)
+			Expect(attributeIntervals).To(HaveLen(2))
+		})
+
+		It("Creates intervals using the default config (10, equal size)", func() {
+			attributeIntervals := classifiers.DiscretizeAttributes(attributeNames, records)
+			Expect(attributeIntervals[0]).To(HaveLen(10))
+
+			Expect(attributeIntervals[0][0].Lower).To(Equal(1.0))
+			Expect(attributeIntervals[0][0].IncludesUpper).To(BeFalse()) // Equal distribution would be true
+
+			Expect(attributeIntervals[0][9].Upper).To(Equal(20.0))
+			Expect(attributeIntervals[0][9].IncludesUpper).To(BeTrue()) // Equal distribution would be true
+
+			Expect(attributeIntervals[1]).To(HaveLen(10))
+			Expect(attributeIntervals[1][0].Lower).To(Equal(1.0))
+			Expect(attributeIntervals[1][0].IncludesUpper).To(BeFalse()) // Equal distribution would be true
+
+			Expect(attributeIntervals[1][9].Upper).To(Equal(10.0))
+			Expect(attributeIntervals[1][9].IncludesUpper).To(BeTrue()) // Equal distribution would be true
+		})
+	})
+
+	Describe("GenerateClassAttributePosteriors", func() {
+		var (
+			classNames, attributeNames []string
+			classAttrIntervals         []probability.Intervals
+			records                    []classifiers.Record
+		)
+
+		BeforeEach(func() {
+			classNames = []string{"a", "b"}
+			attributeNames = []string{"width", "length"}
+			records = []classifiers.Record{
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{1.0, 0.0},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{1.2, 0.1},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{2.3, 0.2},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{2.35, 0.3},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{2.4, 1.0},
+				},
+				// 5 records above
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{3.7, 2.0},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{4.3, 2.0},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{4.7, 2.0},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{5.7, 2.0},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{6.0, 2.0},
+				},
+				// 10 records above
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				// 15 records above
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 10.0},
+				},
+				// 20 records above
+			}
+		})
+
+		JustBeforeEach(func() {
+			classAttrIntervals = classifiers.DiscretizeAttributes(attributeNames, records)
+		})
+
+		It("Creates posteriors for each class, attribute, interval", func() {
+			caps := classifiers.GenerateClassAttributePosteriors(classNames, attributeNames, classAttrIntervals, records)
+			Expect(caps).To(HaveLen(2))
+			Expect(caps[0]).To(HaveLen(len(attributeNames)))
+			Expect(caps[1]).To(HaveLen(len(attributeNames)))
+			for i := 0; i < len(caps); i++ {
+				for j := 0; j < len(attributeNames); j++ {
+					// We expect to have a probability value for each interval
+					Expect(caps[i][j]).To(HaveLen(10))
+				}
+			}
+		})
+
+		It("Creates posteriors conditioned by the class", func() {
+			caps := classifiers.GenerateClassAttributePosteriors(classNames, attributeNames, classAttrIntervals, records)
+			Expect(caps[0][0][0]).NotTo(Equal(caps[1][0][0]))
+		})
+
+		It("Computes the correct posteriors", func() {
+			caps := classifiers.GenerateClassAttributePosteriors(classNames, attributeNames, classAttrIntervals, records)
+
+			Expect(caps[0][0][0]).To(Equal(0.2))
+			Expect(caps[0][0][1]).To(Equal(0.0))
+			Expect(caps[0][0][2]).To(Equal(0.3))
+			Expect(caps[0][0][3]).To(Equal(0.0))
+			Expect(caps[0][0][4]).To(Equal(0.0))
+			Expect(caps[0][0][5]).To(Equal(0.1))
+			Expect(caps[0][0][6]).To(Equal(0.1))
+			Expect(caps[0][0][7]).To(Equal(0.1))
+			Expect(caps[0][0][8]).To(Equal(0.0))
+			Expect(caps[0][0][9]).To(Equal(0.2))
+
+			Expect(caps[0][1][0]).To(Equal(0.4))
+			Expect(caps[0][1][1]).To(Equal(0.1))
+			Expect(caps[0][1][2]).To(Equal(0.5))
+			Expect(caps[0][1][3]).To(Equal(0.0))
+			Expect(caps[0][1][4]).To(Equal(0.0))
+			Expect(caps[0][1][5]).To(Equal(0.0))
+			Expect(caps[0][1][6]).To(Equal(0.0))
+			Expect(caps[0][1][7]).To(Equal(0.0))
+			Expect(caps[0][1][8]).To(Equal(0.0))
+			Expect(caps[0][1][9]).To(Equal(0.0))
+
+			Expect(caps[1][0][0]).To(Equal(1.0))
+			Expect(caps[1][0][1]).To(Equal(0.0))
+			Expect(caps[1][0][2]).To(Equal(0.0))
+			Expect(caps[1][0][3]).To(Equal(0.0))
+			Expect(caps[1][0][4]).To(Equal(0.0))
+			Expect(caps[1][0][5]).To(Equal(0.0))
+			Expect(caps[1][0][6]).To(Equal(0.0))
+			Expect(caps[1][0][7]).To(Equal(0.0))
+			Expect(caps[1][0][8]).To(Equal(0.0))
+			Expect(caps[1][0][9]).To(Equal(0.0))
+
+			Expect(caps[1][1][0]).To(Equal(0.0))
+			Expect(caps[1][1][1]).To(Equal(0.9))
+			Expect(caps[1][1][2]).To(Equal(0.0))
+			Expect(caps[1][1][3]).To(Equal(0.0))
+			Expect(caps[1][1][4]).To(Equal(0.0))
+			Expect(caps[1][1][5]).To(Equal(0.0))
+			Expect(caps[1][1][6]).To(Equal(0.0))
+			Expect(caps[1][1][7]).To(Equal(0.0))
+			Expect(caps[1][1][8]).To(Equal(0.0))
+			Expect(caps[1][1][9]).To(Equal(0.1))
+		})
+	})
+
+	Describe("GenerateClassConditionedPosteriors", func() {
+		var (
+			classNames, attributeNames []string
+			records                    []classifiers.Record
+			intervals                  []probability.Intervals
+			caps                       classifiers.ClassAttributePosteriors
+		)
+
+		BeforeEach(func() {
+			classNames = []string{"a", "b"}
+			attributeNames = []string{"width", "length"}
+			records = []classifiers.Record{
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{1.0, 0.0},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{1.2, 0.1},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{2.3, 0.2},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{2.35, 0.3},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{2.4, 1.0},
+				},
+				// 5 records above
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{3.7, 2.0},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{4.3, 2.0},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{4.7, 2.0},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{5.7, 2.0},
+				},
+				{
+					Class:           0,
+					AttributeValues: wyvern.Vector[float64]{6.0, 2.0},
+				},
+				// 10 records above
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				// 15 records above
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 1.0},
+				},
+				{
+					Class:           1,
+					AttributeValues: wyvern.Vector[float64]{1.0, 10.0},
+				},
+				// 20 records above
+			}
+		})
+
+		JustBeforeEach(func() {
+			intervals = classifiers.DiscretizeAttributes(attributeNames, records)
+			caps = classifiers.GenerateClassAttributePosteriors(classNames, attributeNames, intervals, records)
+		})
+
+		// Skimping a bit here on the tests
+		It("Computes correct values for each class and vector", func() {
+			ccps := classifiers.GenerateClassConditionedPosteriors(classNames,
+				attributeNames,
+				intervals,
+				caps)
+
+			Expect(ccps).To(HaveLen(2))
+			Expect(ccps[0]).To(HaveLen(100))
+			Expect(ccps[1]).To(HaveLen(100))
+
+			// With attributes indexed 0 and 1, each with 10 intervals, vector index 15
+			// has probability P(A1==1)P(A2==5).
+
+			for classIdx, _ := range classNames {
+				for tens := 0; tens < 10; tens++ {
+					for ones := 0; ones < 10; ones++ {
+						expected := caps[classIdx][0][tens] * caps[classIdx][1][ones]
+						Expect(ccps[classIdx][(tens*10)+ones]).To(Equal(expected))
+					}
+				}
+			}
 		})
 	})
 })
